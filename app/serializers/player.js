c2180470ee30f6eb39fc7c1196b0707de93e6fef
John Huynh <john@huynh.io>
2014-04-30 15:59:59 -0400

    Renamed model to record in adapter and serializer.

--- app/serializers/player.js
+++ app/serializers/player.js
@@ -1,9 +1,9 @@
 var PlayerSerializer = DS.RESTSerializer.extend({
   extractSingle: function(store, type, payload, id, requestType) {
-    var modelPayload = JSON.parse(payload.data);
+    var recordPayload = JSON.parse(payload.data);
 
     return this._super.apply(this,
-      [store, type, modelPayload, modelPayload[type.typeKey].id, requestType]);
+      [store, type, recordPayload, recordPayload[type.typeKey].id, requestType]);
   }
 });
 
