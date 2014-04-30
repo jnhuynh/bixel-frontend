5d56e113059f47a7b7fd98be3000b328b1f3b860
John Huynh <john@huynh.io>
2014-04-29 23:52:00 -0400

    Switched back to area.index since it's implicit.

--- app/routes/area/index.js
+++ app/routes/area/index.js
@@ -1,12 +1,12 @@
-var AreaRoute = Ember.Route.extend({
+var AreaIndexRoute = Ember.Route.extend({
   model: function() {
     return this.store.find("area");
   },
 
   setupController: function(controller, model) {
-
+    controller.set("model", model);
   }
 });
 
-export default AreaRoute;
+export default AreaIndexRoute;
 
