268d0bbd1fd9ed41248612775b251349b19edb58
John Huynh <john@huynh.io>
2014-05-13 23:35:55 -0400

    Update: ran ember init after updating ember-cli.

--- config/environment.js
+++ config/environment.js
@@ -1,19 +1,34 @@
 module.exports = function(environment) {
   var ENV = {
-    rootURL: '/',
+    baseURL: '/',
+    locationType: 'auto',
     FEATURES: {
-      // Here you can enable experimental featuers on an ember canary build
+      // Here you can enable experimental features on an ember canary build
       // e.g. 'with-controller': true
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
     }
   };
 
   if (environment === 'development') {
-    ENV.developmentWebSocketHost = "0.0.0.0:3000";
+    // LOG_MODULE_RESOLVER is needed for pre-1.6.0
+    ENV.LOG_MODULE_RESOLVER = true;
+
+    ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_MODULE_RESOLVER = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.developmentWebSocketHost = "0.0.0.0:3000";
   }
 
   if (environment === 'production') {
 
   }
 
-  return JSON.stringify(ENV); // Set in index.html
+  return ENV;
 };
